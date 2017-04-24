d95c03e7bb6de5effbf01271e9c4bb67dcae452e
Taylor Hansen <PeterPan@taylors-mbp-9.bc.edu>
2017-04-24 17:09:01 -0400

    Initial Commit

---
+++ Independent Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Independent Project
+//
+//  Created by Taylor Hansen on 24/04/2017.
+//  Copyright © 2017 Taylor Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
